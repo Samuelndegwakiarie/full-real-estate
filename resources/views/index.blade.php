a6a9a614712a121d7c2a1ab9f18d4edc732fb04e
samuelndegwakiarie <sammiekiarie90@gmail.com>
2023-11-01 00:41:12 +0300

    alot

---
+++ resources/views/index.blade.php
@@ -0,0 +1,156 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Project Genesis</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="{{ asset('style.css') }}">
+
+    <!--fav icon-->
+    <link rel="shortcut icon" type="image.jpg" href="images/logo.jpg">
+
+
+</head>
+
+<body>
+    <section class="header">
+        <div class="container">
+            <img src="images/logo.jpg" alt="logo">
+            <button type="button" class="login-btn">Login </button>
+            <h1>secure your space with us</h1>
+            <p>Book your space in our spacious, comfortable and secure apartments today</p>
+            <p>Have access to fast wifi, hot showers,apartments fitted with 27000w backup generator</p>
+            <div class="input-group">
+                <input type="text" class="form-control" placeholder="what do you want to know ?">
+                <div class="input-group-append">
+                    <button type="button" class="input-group-text btn"> search</button>
+
+                </div>
+            </div>
+        </div>
+
+    </section>
+    <section class="varieties">
+        <h1>Options available</h1>
+        <div class="container">
+            <div class="row">
+
+                <div class="col-md-4">
+                    <div class="varieties-box">
+                        <div class="varieties-img">
+                            <a href="class a.html"> <img src="images/1.jpg" alt="image1"></a>  
+                            <div class="rent">
+                                <p> 45000/= monthly </p>
+                            </div>
+                            <div class="rating">
+                                <i class="fa fa-smile-o"></i>
+                            </div>
+                        </div>
+                        <div class="varieties-details">
+                            <h4>Heading varieties</h4>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio fugiat voluptate quisquam tenetur porro quae tempora vitae? Natus repellendus, optio assumenda neque iste nisi est, quibusdam nemo velit eaque consequatur.</p>
+                        </div>
+                        <span>map maker, moon calender text</span>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="varieties-box">
+                        <div class="varieties-img">
+                            <a href="class b.html"> <img src="images/2.jpg" alt="image2"></a>
+                            <div class="rent">
+                                <p> 55000/= monthly </p>
+                            </div>
+                            <div class="rating">
+                                <i class="fa fa-smile-o"></i>
+
+                            </div>
+                        </div>
+                        <div class="varieties-details">
+                            <h4>Heading varieties</h4>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio fugiat voluptate quisquam tenetur porro quae tempora vitae? Natus repellendus, optio assumenda neque iste nisi est, quibusdam nemo velit eaque consequatur.</p>
+                        </div>
+                        <span>map maker, moon calender text</span>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="varieties-box">
+                        <div class="varieties-img">
+                            <a href="class c.html"><img src="images/4.jpeg" alt="popo"></a>
+                            <div class="rent">
+                                <p> 65000/= monthly </p>
+                            </div>
+                            <div class="rating">
+                                <i class="fa fa-smile-o"></i>
+
+                            </div>
+                        </div>
+                        <div class="varieties-details">
+                            <h4>Heading varieties</h4>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio fugiat voluptate quisquam tenetur porro quae tempora vitae? Natus repellendus, optio assumenda neque iste nisi est, quibusdam nemo velit eaque consequatur.</p>
+                        </div>
+                        <span>map maker, moon calender text</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </section>
+    
+    <section class="banner">
+        <div class="baner-highlights">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-8">
+                        <h2>Early Booking</h2>
+                        <p>while offers last</p>
+                    </div>
+                    <div class="col-md-4">
+                        <button type="button" class="booking-btn">Book</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="residents-feedback">
+        <h1>Residents say</h1>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="residents-review">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo provident quis expedita officia vel sit dignissimos quia rem mollitia magnam, obcaecati iste corporis assumenda omnis similique. A labore obcaecati repudiandae?</p>
+                        <h5>havens</h5>
+                        <small>karatina</small>
+                    </div>
+                    <img src="images/building.jpeg" alt="">
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3">
+                <img src="images/logo.jpg" alt=""class="footer-logo">
+                </div>
+                <div class="col-md-3">
+                    <h4>Features</h4>
+                    <p>Deals & offers</p>
+                    <p>residents offers</p>
+                    <p>Policy & rights</p>
+                </div>
+                <div class="col-md-3">
+                    <h4>Quick contact</h4>
+                    </div>
+                    <div class="col-md-3">
+                        <p>copyright</p>
+                    </div>
+                    <hr>
+                </div>
+            </div>
+     </section>
+</body>
+</html>
